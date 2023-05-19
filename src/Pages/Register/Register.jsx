e9d1b12f13a1af19401f0922fc33ccc6905c1071
jakariamasum <jmphy698@gmail.com>
2023-05-19 21:44:41 +0600

    register functionabilty added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,82 +1,120 @@
-import { useState } from 'react';
-import {AiFillEye,AiFillEyeInvisible} from 'react-icons/ai'
-import {FaGoogle} from 'react-icons/fa'
-import { Link } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Provider/AuthProvider';
+import Swal from 'sweetalert2';
+import { getAuth, updateProfile } from 'firebase/auth'
+const auth = getAuth()
+
 const Login = () => {
-  const [showPassword, setShowPassword] = useState(false);
-  const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const { createUser, logOut } = useContext(AuthContext);
+    const [showPassword, setShowPassword] = useState(false);
+    const [password, setPassword] = useState('');
 
-  const handleTogglePassword = () => {
-    setShowPassword(!showPassword);
-  };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword);
+    };
 
-  const handleGoogleSignIn=()=>{
 
-  }
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 mt-7">
-      <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
-        <h2 className="text-3xl font-semibold mb-2 text-center">Hello from Kids Zone</h2>
-        <small className='flex justify-center mb-4'>Please register here</small>
-        <form>
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700 text-sm font-medium mb-1">Name</label>
-            <input type="text" id="name" name='name' className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 text-sm font-medium mb-1">Email</label>
-            <input type="email" name='email' id="email" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
-          </div>
-          <div className="mb-6 relative">
-            <label htmlFor="password" className="block text-gray-700 text-sm font-medium mb-1">Password</label>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              name='password'
-              value={password}
-              onChange={handlePasswordChange}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-            />
-            <button
-              type="button"
-              className="absolute right-3 top-1/2 transform  text-gray-500 focus:outline-none"
-              onClick={handleTogglePassword}
-            >
-              {showPassword ? (
-                <AiFillEye/>
-              ) : (
-                <AiFillEyeInvisible/>
-              )}
-            </button>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="photoURL" className="block text-gray-700 text-sm font-medium mb-1">Photo URL</label>
-            <input type="text" name='photoURL' id="photoURl" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
-          </div>
-          <div className="mb-6">
-            <input type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600" value="Register"></input>
-          </div>
-        </form>
-        
-        <div className="mb-6">
-          <button onClick={handleGoogleSignIn} className="w-full flex justify-center gap-2 bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:bg-red-600">
-            <FaGoogle size={22}/>
-            <p>Sign in with Google</p>
-          </button>
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+
+    const handleRegister = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const photoUrl = form.photoURL.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(res => {
+                Swal.fire({
+                    position: 'text-center',
+                    icon: 'success',
+                    title: 'Registration Successful! Please login',
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                console.log(res.user)
+                updateProfile(auth.currentUser, {
+                    displayName: name, photoURL: photoUrl
+                }).then(() => {
+                    logOut()
+                        .then(res => {
+                            form.reset();
+                            setPassword('');
+                        })
+                        .catch(error => console.log(error.message))
+
+                    navigate('/login')
+                }).catch((error) => {
+                    console.log(error.message)
+                });
+
+            })
+            .catch(error => console.log(error.message))
+    }
+
+    
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 mt-7">
+            <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
+                <h2 className="text-3xl font-semibold mb-2 text-center">Hello from Kids Zone</h2>
+                <small className='flex justify-center mb-4'>Please register here</small>
+                <form onSubmit={handleRegister}>
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block text-gray-700 text-sm font-medium mb-1">Name</label>
+                        <input type="text" id="name" name='name' className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-700 text-sm font-medium mb-1">Email</label>
+                        <input type="email" name='email' id="email" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <div className="mb-6 relative">
+                        <label htmlFor="password" className="block text-gray-700 text-sm font-medium mb-1">Password</label>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            id="password"
+                            name='password'
+                            value={password}
+                            onChange={handlePasswordChange}
+                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+                        />
+                        <button
+                            type="button"
+                            className="absolute right-3 top-1/2 transform  text-gray-500 focus:outline-none"
+                            onClick={handleTogglePassword}
+                        >
+                            {showPassword ? (
+                                <AiFillEye />
+                            ) : (
+                                <AiFillEyeInvisible />
+                            )}
+                        </button>
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="photoURL" className="block text-gray-700 text-sm font-medium mb-1">Photo URL</label>
+                        <input type="text" name='photoURL' id="photoURl" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <div className="mb-6">
+                        <input type="submit" className="w-full cursor-pointer bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600" value="Register"></input>
+                    </div>
+                </form>
+
+                <div className="text-center">
+                    <p>
+                        Already have an account?{' '}
+                        <Link to="/login" className="text-blue-500 hover:underline">Login here</Link>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div className="text-center">
-          <p>
-            Already have an account?{' '}
-            <Link to="/login" className="text-blue-500 hover:underline">Login here</Link>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,6 +1,6 @@
 const Gallery = () => {
     const images = [
-        'https://img.freepik.com/free-photo/close-up-toy-car-with-easter-eggs_23-2149318378.jpg?w=1060&t=st=1684480705~exp=1684481305~hmac=e3cdd0433fc721383d268503289f943cf9d5b3b1261715d25ab9ffccd9ec6aac',
+        'https://i.ibb.co/7by202m/31-B32-RE8x-AL-SX300-SY300-QL70-FMwebp.webp',
         'https://img.freepik.com/free-photo/close-up-toy-car-with-easter-eggs_23-2149318378.jpg?w=1060&t=st=1684480705~exp=1684481305~hmac=e3cdd0433fc721383d268503289f943cf9d5b3b1261715d25ab9ffccd9ec6aac',
         'https://img.freepik.com/free-photo/close-up-toy-car-with-easter-eggs_23-2149318378.jpg?w=1060&t=st=1684480705~exp=1684481305~hmac=e3cdd0433fc721383d268503289f943cf9d5b3b1261715d25ab9ffccd9ec6aac',
         // ... add more image URLs here
@@ -13,7 +13,7 @@ const Gallery = () => {
                 {images.map((img, indx) => (
                     <div key={indx} className="relative overflow-hidden">
                         <div className="relative">
-                            <img src={img} alt={`Image ${indx + 1}`} className="w-full h-48 object-cover" />
+                            <img src={img} alt={`Image ${indx + 1}`} className="w-full h-full object-cover" />
                             <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-50 bg-black">
                             </div>
                         </div>
