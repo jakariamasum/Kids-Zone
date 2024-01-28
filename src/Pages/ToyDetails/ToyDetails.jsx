d0f255fddd331285785d33510859b12087925311
Jakaria Masum <100533704+jakariamasum@users.noreply.github.com>
2024-01-29 03:37:14 +0600

    Update ToyDetails.jsx

--- src/Pages/ToyDetails/ToyDetails.jsx
+++ src/Pages/ToyDetails/ToyDetails.jsx
@@ -1,10 +1,43 @@
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import useTitlte from '../../hooks/useTitle';
+import Swal from 'sweetalert2';
+import { useContext } from 'react';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const ToyDetails = () => {
     useTitlte('Toy Details')
     const toy = useLoaderData();
+    const {user}=useContext(AuthContext)
     const { picture, sellerName, toyName, sellerEmail, price, quantity, description, rating } = toy;
+
+    const handleBuyClick =  () => {
+            // Prepare toy data to be added to MongoDB
+            const toyData = {
+                ...toy,
+                userEmail: user?.email 
+            };
+            fetch('https://kids-zone-server-weld.vercel.app/addcart', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(toyData)
+        })
+        .then(res=>res.json())
+        .then(data => {
+                console.log(data)
+            })
+            Swal.fire({
+                position: 'text-center',
+                icon: 'success',
+                title: 'Successfully add to cart',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
+
+
+
     return (
         <div className="mt-12 mx-28 flex justify-center">
             <div className="card w-[50%] lg:card-side bg-orange-100  shadow-xl">
@@ -19,6 +52,7 @@ const ToyDetails = () => {
                         <p>Rating: {rating}</p>
                         <p>Description: {description}</p>
                     </div>
+                    <Link onClick={handleBuyClick} to='#' className='bg-blue-500 w-fit px-6 py-2 font-bold text-white rounded-xl'>Buy</Link>
                 </div>
             </div>
         </div>
